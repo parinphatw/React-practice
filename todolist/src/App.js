0e95fcaadb4266bd78011aeb7a0f7ec8c703e71a
ParinphatW <46051881+parinphatw@users.noreply.github.com>
2020-07-05 00:20:11 +0700

    init robofriend

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,34 @@
 import React, {Component} from 'react'
+import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
+    state = {
+        characters: []
+    }
+
+    removeCharacter = index => {
+        const {characters} = this.state
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            })
+        })
+    }
+
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]})
+    }
+
     render() {
-        return (<div className="App">
-                    <h1>Hello, React</h1>
-                </div>
+        const { characters } = this.state
+
+        return (
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
         )    
     }
 }
